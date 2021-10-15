a582271bcc3cae951b06c5dc08e26869eb912496
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-15 13:32:06 -0700

    add light pink sto indicate branch change

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 //document.body.innerHTML = "works"
 
-const steps = Array.from(document.querySelectorAll("form .step"));
-const nextBtn = document.querySelectorAll("form .next-btn");
-const prevBtn = document.querySelectorAll("form .previous-btn");
-const form = document.querySelector("form");
+const steps = Array.from(document.querySelectorAll("form .step")); //grabs all the form data and puts in array
+const nextBtn = document.querySelectorAll("form .next-btn");  //grabs all the next buttons
+const prevBtn = document.querySelectorAll("form .previous-btn"); //grabs all the previous buttons
+const form = document.querySelector("form"); //
+
+
 
 nextBtn.forEach((button) => {
   button.addEventListener("click", () => {
     changeStep("next");
   });
 });
+
 prevBtn.forEach((button) => {
   button.addEventListener("click", () => {
     changeStep("prev");
   });
 });
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -24,6 +28,7 @@ form.addEventListener("submit", (e) => {
     inputs.push({ name, value });
   });
   console.log(inputs);
+  console.log(form)
   form.reset();
   changeStep("next");
 });
