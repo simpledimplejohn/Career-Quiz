a88afdc76310f2546bb6b4d1c4baa36b87f1a420
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-06 11:06:48 -0700

    quiz and buttons now working in quizConsole.js

--- js/quizConsole.js
+++ js/quizConsole.js
@@ -45,55 +45,56 @@ function changeStep(btn) {
 
 //quiz
 
-// let countObject = {a:0,b:0,c:0,d:0,e:0,f:0};
+let countObject = {a:0,b:0,c:0,d:0,e:0,f:0};
 
-// function sortArray(array) {
-//   for(let i = 0; i < array.length; i ++) {
-//     if(array[i] === "a") {
-//       countObject.a ++;
-//     }
-//     if(array[i] === "b") {
-//       countObject.b ++;
-//     }
-//     if(array[i] === "c") {
-//       countObject.c ++;
-//     }
-//     if(array[i] === "d") {
-//       countObject.d ++;
-//     }
-//     if(array[i] === "e") {
-//       countObject.e ++;
-//     }
-//     if(array[i] === "f") {
-//       countObject.f ++;
-//     }
-//   }
-//   for(let key in countObject) {
-//     countObject[key] = Math.floor((countObject[key]/8)*100)
-//   }
-// }
+function sortArray(array) {
+  for(let i = 0; i < array.length; i ++) {
+    if(array[i] === "a") {
+      countObject.a ++;
+    }
+    if(array[i] === "b") {
+      countObject.b ++;
+    }
+    if(array[i] === "c") {
+      countObject.c ++;
+    }
+    if(array[i] === "d") {
+      countObject.d ++;
+    }
+    if(array[i] === "e") {
+      countObject.e ++;
+    }
+    if(array[i] === "f") {
+      countObject.f ++;
+    }
+  }
+  for(let key in countObject) {
+    countObject[key] = Math.floor((countObject[key]/8)*100)
+  }
+}
 
+$('#radio-form').submit(function(event) {
+  event.preventDefault();
 
-// $('#radio-form').submit(function(event) {
-//   event.preventDefault();
-
-//   const q1 = $("input:radio[name=q1]:checked").val();
-//   const q2 = $("input:radio[name=q2]:checked").val();
-//   const q3 = $("input:radio[name=q3]:checked").val();
-//   const q4 = $("input:radio[name=q4]:checked").val();
-//   const q5 = $("input:radio[name=q5]:checked").val();
-//   const q6 = $("input:radio[name=q6]:checked").val();
-//   const q7 = $("input:radio[name=q7]:checked").val();
-//   const q8 = $("input:radio[name=q8]:checked").val();
+  const q1 = $("input:radio[name=q1]:checked").val();
+  const q2 = $("input:radio[name=q2]:checked").val();
+  const q3 = $("input:radio[name=q3]:checked").val();
+  const q4 = $("input:radio[name=q4]:checked").val();
+  const q5 = $("input:radio[name=q5]:checked").val();
+  const q6 = $("input:radio[name=q6]:checked").val();
+  const q7 = $("input:radio[name=q7]:checked").val();
+  const q8 = $("input:radio[name=q8]:checked").val();
   
-//   const questionArray = [q1,q2,q3,q4,q5,q6,q7,q8];
-//   sortArray(questionArray);
+  const questionArray = [q1,q2,q3,q4,q5,q6,q7,q8];
+  sortArray(questionArray);
 
-//   $("#a").prepend(countObject.a);
-//   $("#b").prepend(countObject.b);
-//   $("#c").prepend(countObject.c);
-//   $("#d").prepend(countObject.d);
-//   $("#e").prepend(countObject.e);
-//   $("#f").prepend(countObject.f);
+  $("#a").prepend(countObject.a);
+  $("#b").prepend(countObject.b);
+  $("#c").prepend(countObject.c);
+  $("#d").prepend(countObject.d);
+  $("#e").prepend(countObject.e);
+  $("#f").prepend(countObject.f);
 
-// });
+  changeStep("next");
+
+});
