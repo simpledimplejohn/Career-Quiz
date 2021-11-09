f2aaba9ac4dfe1a743c5367d14ef571572f8ff07
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-09 10:53:40 -0800

    add questions to array and run sortArray() fx, works

--- js/quizConsole.js
+++ js/quizConsole.js
@@ -63,34 +63,45 @@ function sortArray(array) {
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const q1 = document.querySelector('input[name="q1"]:checked')
-  console.log(q1);
+  const q1 = document.querySelector('input[name="q1"]:checked').value;
+  const q2 = document.querySelector('input[name="q2"]:checked').value;
+  const q3 = document.querySelector('input[name="q3"]:checked').value;
+  const q4 = document.querySelector('input[name="q4"]:checked').value;
+  const q5 = document.querySelector('input[name="q5"]:checked').value;
+  const q6 = document.querySelector('input[name="q6"]:checked').value;
+  const q7 = document.querySelector('input[name="q7"]:checked').value;
+  const q8 = document.querySelector('input[name="q1"]:checked').value;
+
+  const questionArray = [q1,q2,q3,q4,q5,q6,q7,q8];
+  sortArray(questionArray)
+  console.log(countObject);
 })
 
-/*
-$('#radio-form').submit(function(event) {
-  event.preventDefault();
 
-  const q1 = $("input:radio[name=q1]:checked").val();
-  const q2 = $("input:radio[name=q2]:checked").val();
-  const q3 = $("input:radio[name=q3]:checked").val();
-  const q4 = $("input:radio[name=q4]:checked").val();
-  const q5 = $("input:radio[name=q5]:checked").val();
-  const q6 = $("input:radio[name=q6]:checked").val();
-  const q7 = $("input:radio[name=q7]:checked").val();
-  const q8 = $("input:radio[name=q8]:checked").val();
+// $('#radio-form').submit(function(event) {
+//   event.preventDefault();
+
+//   const q1 = $("input:radio[name=q1]:checked").val();
+//   const q2 = $("input:radio[name=q2]:checked").val();
+//   const q3 = $("input:radio[name=q3]:checked").val();
+//   const q4 = $("input:radio[name=q4]:checked").val();
+//   const q5 = $("input:radio[name=q5]:checked").val();
+//   const q6 = $("input:radio[name=q6]:checked").val();
+//   const q7 = $("input:radio[name=q7]:checked").val();
+//   const q8 = $("input:radio[name=q8]:checked").val();
   
-  const questionArray = [q1,q2,q3,q4,q5,q6,q7,q8];
-  sortArray(questionArray);
+//   const questionArray = [q1,q2,q3,q4,q5,q6,q7,q8];
+//   console.log(questionArray)
 
-  $("#a").prepend(countObject.a);
-  $("#b").prepend(countObject.b);
-  $("#c").prepend(countObject.c);
-  $("#d").prepend(countObject.d);
-  $("#e").prepend(countObject.e);
-  $("#f").prepend(countObject.f);
+//   sortArray(questionArray);
 
-  changeStep("next");
+//   $("#a").prepend(countObject.a);
+//   $("#b").prepend(countObject.b);
+//   $("#c").prepend(countObject.c);
+//   $("#d").prepend(countObject.d);
+//   $("#e").prepend(countObject.e);
+//   $("#f").prepend(countObject.f);
 
-});
-*/
+//   changeStep("next");
+
+// });
