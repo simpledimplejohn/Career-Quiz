9b53e85e5c209d0ab783722788dcc6a97bcca70e
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-09 15:18:09 -0800

    output of top name and score works, next is second place score

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let countObject = {a:0,b:0,c:0,d:0,e:0,f:0};
-let nameObject = {a:"Social",b:"Organized",c:"Investigative",d:"Creative Problem Solving", e: "Driven", f: "Leadership"}
+let nameArray = ["Social", "Organized", "Investigative", "Creative Problem Solving", "Driving", "Leadership"]
 
 function sortArray(array) {
   for(let i = 0; i < array.length; i ++) {
@@ -33,9 +33,19 @@ function objectArraySort() {
 
 function inputOutput(questionArray) {
   sortArray(questionArray);
-  let numArray = Object.values(countObject);
   console.log(countObject)
-  return numArray
+  let numArray = Object.values(countObject);
+  console.log(numArray)
+  let max = numArray[0];
+  let maxIndex = 0;
+  for (let i = 1; i < numArray.length; i++) {
+    if(numArray[i] > max) {
+      maxIndex = i;
+      max = arr[i];
+    }
+  }
+  
+  return `Top Profile: ${nameArray[maxIndex]} score: ${numArray[maxIndex]}`
 }
 
 console.log(inputOutput(["a","b","c","d","e","f","a","b"]))
