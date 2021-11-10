6fbc0347aa0d90eb07c4aab2cdf5f70bbb18557b
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-10 10:24:23 -0800

    not working for answer of all one

--- js/testFx.js
+++ js/testFx.js
@@ -1,69 +1,67 @@
 // ["a","b","c","d","e","f","a","b"]
-let nameObject = {Social:0, Organized:0, Investigative:0, Creative:0, Driving:0, Leadership:0}
-let firstArray = [];
-let secondArray = [];
-
-function sortArray(array) {
-  for(let i = 0; i < array.length; i ++) {
-    if(array[i] === "a") {
-      nameObject.Social ++;
-    }
-    if(array[i] === "b") {
-      nameObject.Organized ++;
-    }
-    if(array[i] === "c") {
-      nameObject.Investigative ++;
-    }
-    if(array[i] === "d") {
-      nameObject.Creative ++;
-    }
-    if(array[i] === "e") {
-      nameObject.Driving ++;
-    }
-    if(array[i] === "f") {
-      nameObject.Leadership ++;
-    }
-  }
-  // for(let key in countObject) {
-  //   countObject[key] = Math.floor((countObject[key]/8)*100)
-  // }
-}
-function score() {
-  let maxScore = 0;
-  for(const property in nameObject) {
-    if(nameObject[property] > maxScore) maxScore = nameObject[property]
-  }
-  //console.log(maxScore);
-
-  for (const property in nameObject) {
-    if(nameObject[property] === maxScore) {
-      //console.log(`Top Score = ${property} of ${nameObject[property]}`)
-      firstArray.push({[property]:nameObject[property]})
-      nameObject[property] = 0;
-    }
-  }
-
-  //console.log(nameObject)
-  let secondScore = 0;
-  for(const property in nameObject) {
-    if(nameObject[property] > secondScore) secondScore = nameObject[property]
-  }
-  //console.log(secondScore)
-  for(const property in nameObject) {
-    if(nameObject[property] === secondScore) {
-      secondArray.push({[property]:nameObject[property]})
-    }
-  }
-
-  
-}
-
 function inputOutput(questionArray) {
-  sortArray(questionArray);
+  let nameObject = {Social:0, Organized:0, Investigative:0, Creative:0, Driving:0, Leadership:0}
+  let firstArray = [];
+  let secondArray = [];
 
+  function sortArray(array) {
+    for(let i = 0; i < array.length; i ++) {
+      if(array[i] === "a") {
+        nameObject.Social ++;
+      }
+      if(array[i] === "b") {
+        nameObject.Organized ++;
+      }
+      if(array[i] === "c") {
+        nameObject.Investigative ++;
+      }
+      if(array[i] === "d") {
+        nameObject.Creative ++;
+      }
+      if(array[i] === "e") {
+        nameObject.Driving ++;
+      }
+      if(array[i] === "f") {
+        nameObject.Leadership ++;
+      }
+    }
+    // for(let key in countObject) {
+    //   countObject[key] = Math.floor((countObject[key]/8)*100)
+    // }
+  }
+  function score() {
+    let maxScore = 0;
+    for(const property in nameObject) {
+      if(nameObject[property] > maxScore) maxScore = nameObject[property]
+    }
+    //console.log(maxScore);
+    for (const property in nameObject) {
+      if(nameObject[property] === maxScore) {
+        //console.log(`Top Score = ${property} of ${nameObject[property]}`)
+        firstArray.push({[property]:nameObject[property]})
+        nameObject[property] = 0;
+      }
+    }
+    //console.log(nameObject)
+    let secondScore = 0;
+    for(const property in nameObject) {
+      if(nameObject[property] > secondScore) secondScore = nameObject[property]
+    }
+    //console.log(secondScore)
+    for(const property in nameObject) {
+      if(nameObject[property] === secondScore) {
+        secondArray.push({[property]:nameObject[property]})
+      }
+    }
+  }
+  sortArray(questionArray);
   score();
   console.log(`for array [ ${questionArray} ]`)
-  if(firstArray.length > 1) {
+  if (firstArray.length === 1 && secondArray.length === 0) {
+    console.log("Your only top answer was: ")
+    console.log(firstArray)
+  }
+  else if (firstArray.length > 1 && secondArray.length != 0) {
     console.log(`all of these are tied for your top answer`)
     console.log(firstArray)
   }
@@ -75,4 +73,14 @@ function inputOutput(questionArray) {
   }
 }
 
-inputOutput(["a","a","b","b","c","c","d","e"])
+// top three
+// inputOutput(["a","a","b","b","c","c","d","e"])
+// top four
+//inputOutput(["a","a","b","b","c","c","d","d"])
+// // top two
+//inputOutput(["a","a","a","a","b","b","b","b"])
+// // top one
+ inputOutput(["a","a","a","a","a","a","a","a"])
+
+
+
