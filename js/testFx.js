1ba1e67fee227dba8551bda137103f21af1b72fe
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-11 12:41:12 -0800

    test for first and second place working

--- js/testFx.js
+++ js/testFx.js
@@ -3,6 +3,8 @@ function inputOutput(questionArray) {
   let nameObject = {Social:0, Organized:0, Investigative:0, Creative:0, Driving:0, Leadership:0}
   let firstArray = [];
   let secondArray = [];
+  let maxScore = 0
+  let secondScore = 0;
 
   function sortArray(array) {
     for(let i = 0; i < array.length; i ++) {
@@ -30,7 +32,7 @@ function inputOutput(questionArray) {
     // }
   }
   function score() {
-    let maxScore = 0;
+
     for(const property in nameObject) {
       if(nameObject[property] > maxScore) maxScore = nameObject[property]
     }
@@ -43,7 +45,6 @@ function inputOutput(questionArray) {
       }
     }
     //console.log(nameObject)
-    let secondScore = 0;
     for(const property in nameObject) {
       if(nameObject[property] > secondScore) secondScore = nameObject[property]
     }
@@ -57,30 +58,41 @@ function inputOutput(questionArray) {
   sortArray(questionArray);
   score();
   console.log(`for array [ ${questionArray} ]`)
-  if (firstArray.length === 1 && secondArray.length === 0) {
-    console.log("Your only top answer was: ")
-    console.log(firstArray)
+  console.log(`FIRST PLACE SCORE IS: ${maxScore}`)
+  console.log(firstArray)
+  console.log(`  SECOND PLACE SCORE IS: ${secondScore}`)
+  if (secondScore !== 0) {
+    console.log(secondArray)
   }
-  else if (firstArray.length > 1 && secondArray.length != 0) {
-    console.log(`all of these are tied for your top answer`)
-    console.log(firstArray)
-  }
-  else {
-    console.log('your first choice is: ')
-    console.log(firstArray)
-    console.log('the second top answers are:')
-    console.log(secondArray)  
-  }
+  // if (firstArray.length === 1 && secondArray.length === 0) {
+  //   console.log("Your only top answer was: ")
+  //   console.log(firstArray)
+  // }
+  // else if (firstArray.length > 1 && secondArray.length !== 0) {
+  //   console.log(`all of these are tied for your top answer`)
+  //   console.log(firstArray)
+  // }
+  // else {
+  //   console.log('your first choice is: ')
+  //   console.log(firstArray)
+  //   console.log('the second top answers are:')
+  //   console.log(secondArray)  
+  // }
 }
 
 // top three
-// inputOutput(["a","a","b","b","c","c","d","e"])
+//  inputOutput(["a","a","b","b","c","c","d","e"])
 // top four
-//inputOutput(["a","a","b","b","c","c","d","d"])
+// inputOutput(["a","a","b","b","c","c","d","d"])
 // // top two
-//inputOutput(["a","a","a","a","b","b","b","b"])
+// inputOutput(["a","a","a","a","b","b","b","b"])
 // // top one
- inputOutput(["a","a","a","a","a","a","a","a"])
+//inputOutput(["a","a","a","a","a","a","a","a"])
+// one first place five second place
+inputOutput(["a","a","a","b","c","d","e","f"])
+inputOutput(["a","a","a","a","b","c","d","e"])
+inputOutput(["a","a","a","a","a","d","e","f"])
+inputOutput(["a","a","a","a","a","a","e","f"])
 
 
 
