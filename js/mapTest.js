43c4c1cc8ee23832ff6411f528df85499d0cc322
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-13 09:05:47 -0800

    rewriting sortArray method with updated object keys

--- js/mapTest.js
+++ js/mapTest.js
@@ -2,18 +2,71 @@
 //  input:
 const sinScore = [{name: "Lust", score:2},{name:"Sloth", score:2},{name:"Gluttony", score:2}];
 const simpleSinScore = [{Lust:2},{Sloth:2},{Gluttony:2}];
-let newNameArray = [{name: "Social", score: 1},
-                    {name: "Organized", score: 2},
-                    {name: "Investigative", score: 1},
-                    {name: "Creative", score: 1},
-                    {name: "Driving", score: 1},
-                    {name: "Leadership", score: 0},]
-
-function scoreRank() {
+let newNameArray = [{name: "Social", score: 0},
+                    {name: "Organized", score: 0},
+                    {name: "Investigative", score: 0},
+                    {name: "Creative", score: 0},
+                    {name: "Driving", score: 0},
+                    {name: "Leadership", score: 0},
+                    ]
+let firstArray = [];
+let secondArray = [];
+let maxScore = 0
+let secondScore = 0;
+function sortArray(array) {
+  for(let i = 0; i < array.length; i ++) {
+    if(array[i] === "a") {
+      newNameArray.find(i => i.name === 'Social').score ++;
+    }
+    if(array[i] === "b") {
+      newNameArray.find(i => i.name === 'Organized').score ++;
+    }
+    if(array[i] === "c") {
+      newNameArray.find(i => i.name === 'Investigative').score ++;
+    }
+    if(array[i] === "d") {
+      newNameArray.find(i => i.name === 'Creative').score ++;
+    }
+    if(array[i] === "e") {
+      newNameArray.find(i => i.name === 'Driving').score ++;
+    }
+    if(array[i] === "f") {
+      newNameArray.find(i => i.name === 'Leadership').score ++;
+    }
+  }
+  // for(let key in countObject) {
+  //   countObject[key] = Math.floor((countObject[key]/8)*100)
+  // }
+}
+function topTwo() {
+  // finding the max score
   newNameArray.forEach(element => {
-    
+    // console.log(`element: ${element.score}`)
+    if(element.score > maxScore) maxScore = element.score
+  })
+  newNameArray.forEach(element => { //push max scores to array if more than one
+    if(element.score === maxScore) {
+      firstArray.push({[element.name]:[element.score]})
+    }
   })
 }
+
+questionArray = ["a","a","a","a","c","c","c","c"]
+// questionArray = ["a","a","b","b","c","c","d","d"]
+sortArray(questionArray)
+topTwo()
+console.log(firstArray)
+// console.log(newNameArray)
+
+
+
+
+
+
+
+
+/////////////////MAP NOTES////////////////////////
+
 
 htmlArray = []
 
