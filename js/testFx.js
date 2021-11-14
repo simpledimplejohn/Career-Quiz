bda7b08b102a7befbb0e5af060cb0e46f2a6aed3
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-14 09:26:07 -0800

    priting to top-list works

--- js/testFx.js
+++ js/testFx.js
@@ -1,1 +1,7 @@
 // printing array to the dom with a loop
+
+const sinScore = [{name: "Lust", score:2},{name:"Sloth", score:2},{name:"Gluttony", score:2}];
+const simpleSinScore = [{Lust:2},{Sloth:2},{Gluttony:2}];
+
+let topHeader = document.getElementById("top-list");
+topHeader.outerHTML = "Works!"
